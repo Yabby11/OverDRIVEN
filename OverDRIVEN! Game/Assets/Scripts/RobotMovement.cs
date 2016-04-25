b6554b2acdf82c64660caeb94944904db920fda2
DanielJochem <daniel.jochem@gmail.com>
2016-04-25 22:13:26 +1000

    Moved some files around, also added to the RobotMovement script.

--- OverDRIVEN! Game/Assets/Scripts/RobotMovement.cs
+++ OverDRIVEN! Game/Assets/Scripts/RobotMovement.cs
@@ -4,19 +4,84 @@ using System.Collections;
 public class RobotMovement : MonoBehaviour {
 
     public Transform robot;
-    int speed = 40;
+    int speed = 30;
+
+    //public string[] letters = { "a", "b", "c" };
+
+    public float selectionTime = 5.0f;
 
 	// Use this for initialization
 	void Start () {
         robot = this.transform;
+
+        /*int chosenNum = Random.Range(0, letters.Length);
+        Debug.Log(letters[chosenNum]);*/
+
     }
 
     // Update is called once per frame
     void Update() {
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) / speed;
-        robot.position += Vector3.MoveTowards(transform.position, movement, Mathf.Infinity);
+        //To go in GameManager
+        /*if(gameRestarted) {
+         * baseAiVehicle = false;
+         * armouredVehicle = false;
+         * supraVehicle = false;
+         * wrxVehicle = false;
+         * chevelleVehicle = false;
+         * gtrVehicle = false;
+         * stingrayVehicle = false
+         
+            gameRestarted = false;
 
-        Quaternion rot = Quaternion.Euler(0, Input.GetAxis("Mouse X") + 90, 0);
-        robot.rotation = Quaternion.Slerp(transform.rotation, rot, Time.deltaTime / speed);
+          }*/
+        robot.position += Input.GetAxis("Vertical") * -transform.right / speed;
+        robot.Rotate(new Vector3(0, Input.GetAxis("Mouse X"), 0) * Time.deltaTime * speed * 2);
+
+        CastRay();        
+    }
+
+    void CastRay() {
+        RaycastHit hit;
+        if(Physics.Raycast(robot.position, -transform.right, out hit, 500.0f)) {
+            if(hit.transform.tag == "Car") {
+                selectionTime -= Time.deltaTime;
+                if(selectionTime <= 0.0f) {
+                    switch(hit.transform.name) {
+                        case "Base AI" :
+                            //GameManager.Instance.baseAiVehicle = true
+                            break;
+                        case "Armoured":
+                            //GameManager.Instance.armouredVehicle = true
+                            break;
+                        case "Supra":
+                            //GameManager.Instance.supraVehicle = true
+                            break;
+                        case "WRX":
+                            //GameManager.Instance.wrxVehicle = true
+                            break;
+                        case "Chevelle":
+                            //GameManager.Instance.chevelleVehicle = true
+                            break;
+                        case "GTR":
+                            //GameManager.Instance.gtrVehicle = true
+                            break;
+                        case "Stingray":
+                            //GameManager.Instance.stingrayVehicle = true
+                            break;
+                    }
+                    Debug.Log("Selected: " + hit.transform.name);
+                }
+            } else {
+                selectionTime = 5.0f;
+            }
+
+
+            Debug.DrawLine(robot.position, hit.point, Color.red);
+        }
+                
+           // } else {
+              //  Debug.DrawLine(transform.position, hit.point, Color.green);
+            //}
+        //}
     }
 }
