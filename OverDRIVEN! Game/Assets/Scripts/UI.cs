4cf88a27c140980f4f0096ffe210ddff9dabf3c4
Jake Andrews <jakeandrews.karatekid@gmail.com>
2016-04-25 22:07:32 +1000

    Added control changing, timer,  and a temp UI.

---
+++ OverDRIVEN! Game/Assets/Scripts/UI.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class UI : MonoBehaviour {
+
+    public float timer;
+    public Text timerText;
+
+	// Use this for initialization
+	void Start () {
+	
+        
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        timer = GameManager.Instance.timer;
+        timerText.text = ("Timer: " + timer);
+	}
+}
