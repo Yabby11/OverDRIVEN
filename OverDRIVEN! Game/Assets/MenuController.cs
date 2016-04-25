a2b60b634c846bbdecca1a14a881c73ea03f6c7d
Jake Andrews <jakeandrews.karatekid@gmail.com>
2016-04-25 16:16:02 +1000

    Push for merge

---
+++ OverDRIVEN! Game/Assets/MenuController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+    }
+
+    public void start()
+    {
+      
+        
+
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+
+     
+
+    }
+}
