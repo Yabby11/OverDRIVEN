3200c758035fc399cece519102ae34ceb59b77eb
DanielJochem <daniel.jochem@gmail.com>
2016-04-26 00:33:12 +1000

    First of all, added comments to both the RobotMovement and GameManager scripts.
    In the GameManager script:
        1. I updated it to be less of a clustermess, spacing out variables into sections.
        2. Changed the way the timer works (so that we don't get multiple changes straight up, keeping WASD until the 30 second mark).
        3. Added in vehicle variables to handle RobotController stuff.
        4. Changed the way the controls are changed, so that the controls are continuously reset until none of the controls are assigned to the same key.
    
    In the RobotController script:
        1. Cleaned out commented out code that I used to help Jake with the GameManager script.
        2. Uncommented the stuff that needed to be commented out until the GameManager was ready for it (car choosing stuff).
        3. Added an on-screen timer that counts down the seconds till the car the Raycast has hit, reaches 0.

--- OverDRIVEN! Game/Assets/Scripts/GameManager.cs
+++ OverDRIVEN! Game/Assets/Scripts/GameManager.cs
@@ -3,48 +3,92 @@ using System.Collections;
 
 public class GameManager : SingletonBehaviour<GameManager> {
 
-    public float timer;
-    public string forwardC;
-    public string backwardC;
-    public string leftC;
-    public string rightC;
-    public string[] arrayOfKeys = {"a", "b", "c", "d", "e", "f", "g", " h", "i", "j", "k", "l", "m", " n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+    //Cars to choose from.
+    public bool baseAiVehicle, 
+                armouredVehicle,  
+                supraVehicle,
+                wrxVehicle,
+                chevelleVehicle,
+                gtrVehicle,
+                stingrayVehicle;
 
-    // Use this for initialization
-    void Start () {
-        Debug.Log("Start");
-        
+    //Car is selected, game can now begin
+    /*@*@*@* Add when you crash, set to false *@*@*@*/
+    public bool gameCanBegin;
+    
+    //Game is over, go back to car selection screen.
+    /*@*@*@* Add when you crash, set to true *@*@*@*/
+    public bool gameRestarted;
+
+    //The hacker timer.
+    public float timer;
+
+    //The 4 directions you can move in.
+    public string forwardC,
+                  backwardC,
+                  leftC,
+                  rightC;
+
+    //The different keys that the Failsecure system can change your controls to.
+    public string[] arrayOfKeys = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+
+
+    void Start () {        
+        //Initial set controls to WASD.
         forwardC = "w";
         backwardC = "s";
         leftC = "a";
         rightC = "d";
     }
 
-	
-	// Update is called once per frame
-	void Update () {
-        timer += Time.deltaTime;
-        hackerControls();
+    void Update() {
+        if(gameCanBegin) {
+            timer += Time.deltaTime;
+
+            //If the counter is at a 30 second mark,
+            if(timer % 30 <= 0.02f && timer > 1f) {
+                //Change the controls.
+                HackerControls();
+            }
+
+            //If the game is over e.g. you crashed, restart the game.
+            if(gameRestarted) {
+                ResetChosenCar();
+                gameRestarted = false;
+            }
+        }
     }
 
+    //If the game has asked to be restarted,
+    void ResetChosenCar() {
+        //Delete the current car from being chosen, so you can re-choose.
+        baseAiVehicle = armouredVehicle =
+        supraVehicle = wrxVehicle =
+        chevelleVehicle = gtrVehicle = 
+        stingrayVehicle = false;
+    }
 
+    //When reassigning controls,
     int GetRandResult() {
+        //Grab a random letter from the array,
         int result = Random.Range(0, arrayOfKeys.Length);
+
+        //and return it.
         return result;
     }
 
+    void HackerControls() {
+        //Setting the controls to the same value, so that the while loop can begin.
+        forwardC = backwardC = leftC = rightC = ">:)";
 
-    void hackerControls() {
-        if(timer % 30 < 0.1f) {
-            string rightC = arrayOfKeys[GetRandResult()];
-            string leftC = arrayOfKeys[GetRandResult()];
-            string forwardC = arrayOfKeys[GetRandResult()];
-            string backwardC = arrayOfKeys[GetRandResult()];
-
-            Debug.Log(rightC);
-            Debug.Log(leftC);
-            Debug.Log(forwardC);
-            Debug.Log(backwardC);
+        //If any of the directions are assigned the same letter as another,
+        while(forwardC == backwardC || forwardC == leftC || forwardC == rightC || backwardC == leftC || backwardC == rightC || leftC == rightC) {
+            //Reassign all letters till they are all different from each other.
+            forwardC = arrayOfKeys[GetRandResult()];
+            backwardC = arrayOfKeys[GetRandResult()];
+            leftC = arrayOfKeys[GetRandResult()];
+            rightC = arrayOfKeys[GetRandResult()];
+            Debug.Log("Forward: " + forwardC + ", Backward: " + backwardC + ", Left: " + leftC + ", Right: " + rightC);
         }
     }
 }
--- OverDRIVEN! Game/Assets/Scripts/RobotController..cs
+++ OverDRIVEN! Game/Assets/Scripts/RobotController..cs
@@ -3,85 +3,81 @@ using System.Collections;
 
 public class RobotMovement : MonoBehaviour {
 
+    //Get the transform of the robot.
     public Transform robot;
+
+    //Set the speed that will be used in the movement and rotation calculations.
     int speed = 30;
 
-    //public string[] letters = { "a", "b", "c" };
+    //Set up the Raycast.
+    RaycastHit hit;
 
+    //How long the laser has been on the car, after 5 seconds, signifying that they want to choose that one.
     public float selectionTime = 5.0f;
 
-	// Use this for initialization
 	void Start () {
+        //Set the robot transform to the variable.
         robot = this.transform;
-
-        /*int chosenNum = Random.Range(0, letters.Length);
-        Debug.Log(letters[chosenNum]);*/
-
     }
 
-    // Update is called once per frame
     void Update() {
-        //To go in GameManager
-        /*if(gameRestarted) {
-         * baseAiVehicle = false;
-         * armouredVehicle = false;
-         * supraVehicle = false;
-         * wrxVehicle = false;
-         * chevelleVehicle = false;
-         * gtrVehicle = false;
-         * stingrayVehicle = false
-         
-            gameRestarted = false;
-
-          }*/
+        //Movement.
         robot.position += Input.GetAxis("Vertical") * -transform.right / speed;
+
+        //Rotation.
         robot.Rotate(new Vector3(0, Input.GetAxis("Mouse X"), 0) * Time.deltaTime * speed * 2);
 
+        //Laser.
         CastRay();        
     }
 
     void CastRay() {
-        RaycastHit hit;
         if(Physics.Raycast(robot.position, -transform.right, out hit, 500.0f)) {
+            //If the Raycast hits a car,
             if(hit.transform.tag == "Car") {
+                //Start counting down to 0 form 5.
                 selectionTime -= Time.deltaTime;
+                //If the countdown reaches 0,
                 if(selectionTime <= 0.0f) {
+                    //What car did you laser down? Set it to the selected car.
                     switch(hit.transform.name) {
                         case "Base AI" :
-                            //GameManager.Instance.baseAiVehicle = true
+                            GameManager.Instance.baseAiVehicle = true;
                             break;
                         case "Armoured":
-                            //GameManager.Instance.armouredVehicle = true
+                            GameManager.Instance.armouredVehicle = true;
                             break;
                         case "Supra":
-                            //GameManager.Instance.supraVehicle = true
+                            GameManager.Instance.supraVehicle = true;
                             break;
                         case "WRX":
-                            //GameManager.Instance.wrxVehicle = true
+                            GameManager.Instance.wrxVehicle = true;
                             break;
                         case "Chevelle":
-                            //GameManager.Instance.chevelleVehicle = true
+                            GameManager.Instance.chevelleVehicle = true;
                             break;
                         case "GTR":
-                            //GameManager.Instance.gtrVehicle = true
+                            GameManager.Instance.gtrVehicle = true;
                             break;
                         case "Stingray":
-                            //GameManager.Instance.stingrayVehicle = true
+                            GameManager.Instance.stingrayVehicle = true;
                             break;
                     }
                     Debug.Log("Selected: " + hit.transform.name);
+                    GameManager.Instance.gameCanBegin = true;
                 }
             } else {
+                //If you moved your laser before the time was up, reset the countdown time.
                 selectionTime = 5.0f;
             }
 
-
+            //So in the Editor, we can see the Raycast hitting the cars.
             Debug.DrawLine(robot.position, hit.point, Color.red);
         }
-                
-           // } else {
-              //  Debug.DrawLine(transform.position, hit.point, Color.green);
-            //}
-        //}
+    }
+
+    void OnGUI() {
+        if(selectionTime < 5.0f && selectionTime > -0.01f)
+        GUI.Label(new Rect(10, 10, 400, 90), "Selecting " + hit.transform.name + " in: " + selectionTime.ToString("F2") + " seconds");
     }
 }
